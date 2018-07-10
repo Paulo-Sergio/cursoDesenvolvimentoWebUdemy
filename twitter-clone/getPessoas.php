0319dc88555d67641b6a8056c8edc5c6e5690d46
Paulo-Sergio <paulofranca.contato@gmail.com>
2018-07-10 17:58:42 -0300

    finalizando logica para exibição do botão de seguir e deixar de seguir pessoas

--- twitter-clone/getPessoas.php
+++ twitter-clone/getPessoas.php
@@ -15,7 +15,10 @@
   $pdo = new db();
   $connection = $pdo->conectaMysql();
 
-  $sql = "SELECT * FROM usuarios WHERE usuario LIKE :nomePessoa AND id <> :idUsuario";
+  $sql = "SELECT u.*, us.* 
+          FROM usuarios AS u 
+          LEFT JOIN usuarios_seguidores AS us ON (us.id_usuario = :idUsuario AND u.id = us.seguindo_id_usuario) 
+          WHERE usuario LIKE :nomePessoa AND id <> :idUsuario";
   $stmt = $connection->prepare($sql);
   $stmt->bindValue(':nomePessoa', '%'.$nomePessoa.'%');
   $stmt->bindValue(':idUsuario', $idUsuario);
@@ -23,11 +26,21 @@
   if($stmt->rowCount() > 0) {
     $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
     foreach($usuarios as $u) {
+      $estaSeguindoUsuarioSN = isset($u['id_usuario_seguidor']) && !empty($u['id_usuario_seguidor']) ? 'S' : 'N';
+      $btnSeguirDisplay = 'block';
+      $btnDeixarDeSeguirDisplay = 'block';
+      
+      if ($estaSeguindoUsuarioSN == 'N') {
+        $btnDeixarDeSeguirDisplay = 'none';
+      } else {
+        $btnSeguirDisplay = 'none';
+      }
+
       echo '<a href="#" class="list-group-item">';
         echo '<strong>'.$u['usuario'].'</strong> <small> - '.$u['email'].' </small>';
         echo '<p class="list-group-item-text pull-right">';
-          echo '<button type="button" id="btn_seguir_'.$u['id'].'" class="btn btn-primary btn_seguir" data-id_usuario="'.$u['id'].'">Seguir</button>';
-          echo '<button type="button" id="btn_deixar_seguir_'.$u['id'].'" class="btn btn-default btn_deixar_seguir" data-id_usuario="'.$u['id'].'" style="display:none">Deixar de Seguir</button>';
+          echo '<button type="button" id="btn_seguir_'.$u['id'].'" class="btn btn-primary btn_seguir" data-id_usuario="'.$u['id'].'" style="display:'.$btnSeguirDisplay.'">Seguir</button>';
+          echo '<button type="button" id="btn_deixar_seguir_'.$u['id'].'" class="btn btn-default btn_deixar_seguir" data-id_usuario="'.$u['id'].'" style="display:'.$btnDeixarDeSeguirDisplay.'">Deixar de Seguir</button>';
         echo '</p>';
         echo '<div class="clearfix"></div>';
       echo '</a>';
